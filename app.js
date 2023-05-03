a94833b17ec54f9d2e275026cbd4e70995494b7e
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-05-03 13:10:27 -0500

    feat: :sparkles: Selection

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 import myHeader from "./components/my-header.js";
 import mySection from "./components/my-section.js";
+import myNav from "./components/my-nav.js";
+import mySelection from "./components/my-selection.js";
---
+++ components/my-selection.js
@@ -0,0 +1,18 @@
+let pathName = new URL(import.meta.url).pathname;
+let name = pathName.split("/").pop().replace(".js", "");
+export default class mySelection extends HTMLElement{
+    static async components(){
+        return await(await fetch(pathName.replace(".js", ".html"))).text();
+    }
+
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
+        Promise.resolve(mySelection.components()).then(html=>{
+            this.shadowRoot.innerHTML = html
+        })
+        console.log("Etiqueta renderizada y configurada");
+    }
+}
+mySelection.components()
+customElements.define(name, mySelection)
