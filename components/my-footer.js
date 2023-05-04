19c198838058e9bd1b46d45ded149045627466a0
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-05-04 09:31:37 -0500

    feat: :sparkles: handleEvent, footer

--- components/my-footer.js
+++ components/my-footer.js
@@ -9,8 +9,18 @@ export default class myFooter extends HTMLElement{
         super();
         //El attachShadow lo reserva lo aisla
         this.attachShadow({mode: "open"});
+    }
+    handleEvent(e){
+        (e.type === "click") ? this.enviarWorker(e) : undefined
+    }
+    enviarWorker(e){
+        console.log(`El ${this.myBtn.value} ha sido tocado`);
+    }
+    connectedCallback(){
         Promise.resolve(myFooter.components()).then(html=>{
             this.shadowRoot.innerHTML = html
+            this.myBtn = this.shadowRoot.querySelector(".btn")
+            this.myBtn.addEventListener("click", this.handleEvent.bind(this))
         })
         console.log("Etiqueta renderizada y configurada");
     }
